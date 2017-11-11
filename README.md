7aa044b41803fed95e1c3b3ce2ded10a3f93a251
Julia <jklugher@wellesley.edu>
2017-11-10 21:31:26 -0500

    add template stuff

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# whack2017
+# Horizons Hackathon Template.
+
+## Steps 1: Get your project set-up
+You have two ways of working on your project: Local and Gomix.
+
+### Local development
+1. Clone the repo.
+1. Create a `env.sh` file that contains:
+
+    ```
+    export MONGODB_URI='YOUR URI';
+    export SECRET='YOUR SECRET'
+    ```
+
+1. Run `source .env`, and you are good to go!
+
+### Glitch Development
+
+1. Go to https://glitch.com/edit/#!/horizons-hackathon and click
+  `Remix this 🎤`
+1. Select `.env` on the left panel, add your `MONGODB_URI` and `SECRET`
+1. Click `Show` at the top to preview your app!
---
+++ routes/routes.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var models = require('../models');
+var User = models.User;
+
+//////////////////////////////// PUBLIC ROUTES ////////////////////////////////
+// Users who are not logged in can see these routes
+
+router.get('/', function(req, res, next) {
+  res.render('home');
+});
+
+///////////////////////////// END OF PUBLIC ROUTES /////////////////////////////
+
+router.use(function(req, res, next){
+  if (!req.user) {
+    res.redirect('/login');
+  } else {
+    return next();
+  }
+});
+
+//////////////////////////////// PRIVATE ROUTES ////////////////////////////////
+// Only logged in users can see these routes
+
+router.get('/protected', function(req, res, next) {
+  res.render('protectedRoute', {
+    username: req.user.username,
+  });
+});
+
+///////////////////////////// END OF PRIVATE ROUTES /////////////////////////////
+
+module.exports = router;
